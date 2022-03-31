39dfeb0f2c1e5046bb03560f6a7164b418701cce
glennDittmann <glenn.dittmann@posteo.de>
2022-03-31 13:45:18 +0200

    add file to store each cards values

---
+++ cardConstants.js
@@ -0,0 +1,11 @@
+export const CONDOM = 
+{
+    "name": "Condom",
+    "effectiveness": 8.75,  // measured with Pearl-Index; atm source is https://de.wikipedia.org/wiki/Empf%C3%A4ngnisverh%C3%BCtung, using the mean of (2,12) & (7,14)
+    "STI-protection": 10.0,
+    "cost": 1.0,
+    "accessibility": 3.0,
+    "side-effects": 0.0,
+    "ease-of-use": 1.0,
+    "reversability": 10.0,
+}
