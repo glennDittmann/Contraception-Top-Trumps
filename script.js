92e3decf59e340e9506ed59ee7edb80cbae3cab8
glennDittmann <glenn.dittmann@posteo.de>
2022-04-04 21:13:12 +0200

    minor changes

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 import Deck from './cardDeck.js'
-import {CONDOM} from './cardConstants.js'
+import {ANTI_BABY_PILL, COITUS_INTERRUPTUS, CONDOM, COPPER_COIL, DIAPHRAGMA,
+        FERTILITY_CYCLE, STERILIZATION, THE_SWITCH} from './cardConstants.js'
 
-var cards = [CONDOM]
+var cards = [CONDOM, DIAPHRAGMA, ANTI_BABY_PILL, COITUS_INTERRUPTUS, COPPER_COIL, STERILIZATION,
+             FERTILITY_CYCLE, THE_SWITCH]
 
 const deck = new Deck(cards)
+deck.shuffle()
+let { cards1, cards2 } = deck.split()
+var deck1 = new Deck(cards1)
+var deck2 = new Deck(cards2)
 
-console.log(deck.cards)
+deck1.display()
+
+console.log(deck1.cards[0]["path"])
+console.log(deck2.cards)
