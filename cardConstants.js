4a9b14d7c6194f80c75fda07425f468746b92fdc
Lukas E Meindl <git@lukasmeindl.at>
2022-05-07 13:16:42 +0200

    Minor layouting

--- cardConstants.js
+++ cardConstants.js
@@ -1,14 +1,14 @@
 const CONDOM = 
 {
     "name": "Condom",
+    "path": "../assets/kondom.svg",
     "effectiveness": 8.75,  // measured with Pearl-Index; atm source is https://de.wikipedia.org/wiki/Empf%C3%A4ngnisverh%C3%BCtung, using the mean of (2,12) & (7,14)
     "STI-protection": 10.0,
     "cost": 1.0,
     "accessibility": 3.0,
     "side-effects": 0.0,
     "ease-of-use": 1.0,
-    reversibility: 10.0,
-    path: "../assets/kondom.svg"
+    "reversibility": 10.0,
 }
 
 const DIAPHRAGMA = 
@@ -97,8 +97,6 @@ const THE_SWITCH =
 
 // other methods: 
 // copper chain,
-// fertility cycle
-// the switch (!!!)
 
 
 export {CONDOM, ANTI_BABY_PILL, COITUS_INTERRUPTUS, COPPER_COIL,
