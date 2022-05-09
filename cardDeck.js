6daf488702b653afbda5fa03c52fcbf7f071464f
Lukas E Meindl <git@lukasmeindl.at>
2022-05-09 11:46:24 +0200

    Refactoring, adding style changes, highlighting / click, started with scripting behavior for game

--- cardDeck.js
+++ cardDeck.js
@@ -1,8 +1,9 @@
-export default class Deck 
+export default class Deck
 {
-    constructor(cards) 
+    constructor(cards, gameLogic)
     {
         this.cards = cards
+        this.gameLogic = gameLogic
     }
 
     getNumCards()
@@ -25,27 +26,81 @@ export default class Deck
 
     split()
     {
-        var deck1 = new Deck(this.cards.slice(0, this.getNumCards()/2))
-        var deck2 = new Deck(this.cards.slice(this.getNumCards()/2, this.getNumCards()))
+        const deck1 = new Deck(this.cards.slice(0, this.getNumCards() / 2), this.gameLogic);
+        const deck2 = new Deck(this.cards.slice(this.getNumCards() / 2, this.getNumCards()), this.gameLogic);
 
         return {deck1, deck2}
     }
 
-    display(id)
+    updateCardHolder(id, gameLogic)
     {
+        let currentCardData = this.cards[0];
+
         // displays the top card, here the first card in the array
         const img = document.createElement('img');
-        img.src = this.cards[0]["path"]
+        img.src = currentCardData["path"]
+
         if(this.cards[0].hasOwnProperty("path"))
         {
-            img.src = this.cards[0]["path"]
+            img.src = currentCardData["path"]
         }
         else
         {
             img.src = "../assets/unknown.svg"
         }
 
-        var holder = document.getElementById(id)
-        holder.appendChild(img)
+        const cardHolder = document.getElementById(id);
+        cardHolder.appendChild(img)
+
+        let children = cardHolder.children;
+        this.addOverlayImg(cardHolder, "effectiveness", gameLogic);
+        this.addOverlayImg(cardHolder, "STI-protection", gameLogic);
+        this.addOverlayImg(cardHolder, "cost", gameLogic);
+        this.addOverlayImg(cardHolder, "accessibility", gameLogic);
+        this.addOverlayImg(cardHolder, "side-effects", gameLogic);
+
+        children[0].innerHTML = currentCardData["effectiveness"]
+        children[1].innerHTML = currentCardData["STI-protection"]
+        children[2].innerHTML = currentCardData["cost"]
+        children[3].innerHTML = currentCardData["accessibility"]
+        children[4].innerHTML = currentCardData["side-effects"]
+    }
+
+    addOverlayImg(cardHolder, className, gameLogic)
+    {
+        function handleAttributeHover(overlayImg, className)
+        {
+            overlayImg.src = "../assets/attribute_hovered.svg"
+            overlayImg.className = className + "_hovered"
+        }
+
+        function handleAttributeClick(overlayImg, className, gameLogic)
+        {
+            overlayImg.src = "../assets/attribute_selected.svg"
+            overlayImg.className = className + "_selected"
+
+            const increment = 0.045;
+            let opacity = 0;
+            overlayImg.style.opacity = opacity
+            const instance = window.setInterval(function (overlayImg)
+                {
+                    overlayImg.style.opacity = opacity
+                    opacity = opacity + increment;
+                    if (opacity > 1)
+                    {
+                        window.clearInterval(instance);
+                        gameLogic.chooseAttribute(className)
+                    }
+                }, 10, overlayImg);
+        }
+
+        const id = cardHolder.id + "_" + className + "_overlay"
+
+        const overlayImg = document.createElement('img')
+        overlayImg.id = id
+        overlayImg.onclick = function(){handleAttributeClick(overlayImg, className, gameLogic)}
+        overlayImg.onmouseenter = function(){handleAttributeHover(overlayImg, className)}
+        cardHolder.appendChild(overlayImg)
+        handleAttributeHover(overlayImg, className)
     }
 }
--- script.js
+++ script.js
@@ -2,16 +2,67 @@ import Deck from './cardDeck.js'
 import {ANTI_BABY_PILL, COITUS_INTERRUPTUS, CONDOM, COPPER_COIL, DIAPHRAGMA,
         FERTILITY_CYCLE, STERILIZATION, THE_SWITCH} from './cardConstants.js'
 
+
+export default class GameLogic
+{
+    init(deck)
+    {
+        deck.shuffle()
+        const decks = deck.split();
+        this.playerDeck = decks.deck1
+        this.aiDeck = decks.deck2
+
+        const discardDeck = new Deck({}, this);
+        this.discardDeck = discardDeck
+
+
+        this.playerDeck.updateCardHolder('card1', this)
+        this.aiDeck.updateCardHolder('card2', this)
+
+        console.log(this.playerDeck.cards)
+        console.log(this.aiDeck.cards)
+    }
+
+    chooseAttribute(name)
+    {
+        if(name == "effectiveness")
+        {
+
+        }
+
+        if(name == "STI-protection")
+        {
+
+        }
+
+        if(name == "cost")
+        {
+
+        }
+
+        if(name == "accessibility")
+        {
+
+        }
+
+        if(name == "side-effects")
+        {
+
+        }
+
+        this.playerDeck.cards.shift()
+        this.playerDeck.updateCardHolder('card1', this)
+
+        this.aiDeck.cards.shift()
+        this.aiDeck.updateCardHolder('card2', this)
+    }
+}
+
+
+const gameLogic = new GameLogic();
+
 const cards = [CONDOM, DIAPHRAGMA, ANTI_BABY_PILL, COITUS_INTERRUPTUS, COPPER_COIL, STERILIZATION,
     FERTILITY_CYCLE, THE_SWITCH];
+const deck = new Deck(cards, gameLogic)
 
-const deck = new Deck(cards)
-deck.shuffle()
-
-let { deck1, deck2 } = deck.split()
-
-deck1.display('card1')
-deck2.display('card2')
-
-console.log(deck1.cards)
-console.log(deck2.cards)
+gameLogic.init(deck);
