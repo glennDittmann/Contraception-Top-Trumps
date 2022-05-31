0ba97c5a3fd7dfc8ac708b34bf38cf5264d3c50c
glennDittmann <glenn.dittmann@posteo.de>
2022-05-31 21:02:07 +0200

    fill lib cards with corresponding values

--- lib-script.js
+++ lib-script.js
@@ -1,3 +1,7 @@
+import Deck from './cardDeck.js'
+import {CONDOM, MINI_PILL, PATCH, PREP, COPPER_COIL,
+    VASECTOMY, TUBENLIGATUR, INJECTION, FERTILITY_CYCLE, DIAPHRAGMA, ATTRIBUTES} from './cardConstants.js'
+
 function resizeLibWindow() {
     const scaleWidth = window.innerWidth / 1920
     const scaleHeight = window.innerHeight / 1080;
@@ -7,4 +11,27 @@ function resizeLibWindow() {
     cardLib.style.transform = 'scale(' + scale.toString() + ', ' + scale.toString() + ')';
 }
 
+function fillCardValues(card) {
+    console.log(card.name);
+    console.log(card.name + "-" + ATTRIBUTES[0]);
+    const effectiveness = document.getElementById(card.name + "-" + ATTRIBUTES[0]);
+    const stiProtection = document.getElementById(card.name + "-" + ATTRIBUTES[1]);
+    const cost = document.getElementById(card.name + "-" + ATTRIBUTES[2]);
+    const accessibility = document.getElementById(card.name + "-" + ATTRIBUTES[3]);
+    const sideEffects = document.getElementById(card.name + "-" + ATTRIBUTES[4]);
+
+    effectiveness.innerHTML = card[ATTRIBUTES[0]];
+    stiProtection.innerHTML = card[ATTRIBUTES[1]];
+    cost.innerHTML = card[ATTRIBUTES[2]];
+    accessibility.innerHTML = card[ATTRIBUTES[3]];
+    sideEffects.innerHTML = card[ATTRIBUTES[4]];
+}
+
+const cards = [CONDOM, MINI_PILL, PATCH, PREP, COPPER_COIL,
+               VASECTOMY, TUBENLIGATUR, INJECTION, FERTILITY_CYCLE, DIAPHRAGMA];
+
+
+const deck = new Deck(cards);
+console.log(deck.cards);
+cards.forEach(fillCardValues);
 //window.onresize = resizeLibWindow;
