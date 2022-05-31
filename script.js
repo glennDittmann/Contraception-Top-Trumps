218270e4358efbcf100ed7456688935a0af630ad
glennDittmann <glenn.dittmann@posteo.de>
2022-05-31 21:00:41 +0200

    change declaration order

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ import GameLogic from './gameLogic.js';
 import {CONDOM, FEMALE_CONDOM, COMBINED_PILL, MINI_PILL, COITUS_INTERRUPTUS, PATCH, PREP, COPPER_COIL,
     VASECTOMY, TUBENLIGATUR, INJECTION, FERTILITY_CYCLE, DIAPHRAGMA} from './cardConstants.js'
 
-const gameLogic = new GameLogic();
-
+    
 const cards = [CONDOM, FEMALE_CONDOM, COMBINED_PILL, MINI_PILL, COITUS_INTERRUPTUS, PATCH, PREP, COPPER_COIL,
-    VASECTOMY, TUBENLIGATUR, INJECTION, FERTILITY_CYCLE, DIAPHRAGMA];
-
+        VASECTOMY, TUBENLIGATUR, INJECTION, FERTILITY_CYCLE, DIAPHRAGMA];
+        
+const gameLogic = new GameLogic();
 const deck = new Deck(cards, gameLogic);
 
 gameLogic.init(deck);
